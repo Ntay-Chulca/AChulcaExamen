79079ad45ea5be35cad92c14dc9377dbb7193744
master <master@SMLAB306PC04_23>
2023-06-23 20:37:59 -0500

    Agregar archivos de proyecto.

---
+++ AChulcaExamen/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AChulcaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            string user = "estudiante2023";
+            string password = "uisrael2023";
+
+            if (txtUser.Text == user && txtPassword.Text == password)
+            {
+                Navigation.PushAsync(new Registro(user));
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario o Contraseña Incorrecto", "Cerrar");
+            }
+        }
+
+
+    }
+}
---
+++ AChulcaExamen/Registro.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AChulcaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        string usuario;
+        public Registro(string usuario)
+        {
+            InitializeComponent();
+            lblUser.Text = "USUARIO CONECTADO" + " " + "ACHULCA" + " " + usuario;
+            this.usuario = usuario;
+        }
+
+
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnPagoM_Clicked(object sender, EventArgs e)
+        {
+            double Monto = Convert.ToDouble(txtMonto.Text);
+            double PagoMensual = (1500 - Monto) / 4 * 1.04;
+            txtPagoM.Text = PagoMensual.ToString();
+        }
+
+
+        private void btnResumen_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
